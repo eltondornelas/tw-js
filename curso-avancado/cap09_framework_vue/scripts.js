38acbf735a507f14625e48a2c2608a38c670183d
Elton Dornelas <eltondornelas@gmail.com>
2020-10-17 15:48:09 -0300

    Adicionando Cap09-Conhecendo o Framework Vue

---
+++ curso-avancado/cap09_framework_vue/scripts.js
@@ -0,0 +1,17 @@
+new Vue({
+	// el -> element
+	el: '#app',
+	data: {
+		name: '',
+		list: [
+			{ name: 'javascript' },
+			{ name: 'vue' }
+		]
+	},
+	methods: {
+		insert: function () {
+			this.list.push({ name: this.name });
+			this.name = '';
+		}
+	}
+})
